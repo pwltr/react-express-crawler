8b2b915747ef4e87e4f78117c7459054c5928dd6
backdot <philippwalter@pm.me>
2019-12-08 16:26:18 +0100

    build initial frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import './App.css'
 
 const App = () => {
+  const [url, setURL] = useState('')
+
+  const onChange = event => {
+    setURL(event.target.value)
+  }
+
+  const onSubmit = event => {
+    event.preventDefault()
+    axios.get(`http://localhost:3000?query=${url}`).then(response => {
+      console.log('response', response)
+    })
+  }
+
   return (
-    <>Hello World</>
+    <div className="container">
+      <h1 className="title">Website Analyzer</h1>
+      <form className="form" onSubmit={onSubmit}>
+        <input className="input" type="text" value={url} placeholder="Type in your URL" autoFocus onChange={onChange} />
+        <button className="button" type="submit">
+          Analyze
+        </button>
+      </form>
+    </div>
   )
 }
 
