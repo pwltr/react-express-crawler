e19a806c888ca0a417fd5e012f65bd3de527f90d
backdot <philippwalter@pm.me>
2019-12-09 22:21:26 +0100

    fix largest image logic, fix links logic, fix error handling, refactoring

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const url = require('url')
 const cors = require('cors')
 const rp = require('request-promise')
 const cheerio = require('cheerio')
+const probeImageSize = require('probe-image-size')
+const checkBrokenLinks = require('check-broken-links')
 
 const app = express()
 const port = 3000
@@ -19,30 +22,32 @@ const UrlRegex = new RegExp(
 )
 
 app.get('/', (req, res) => {
-  const url = req.query.url
+  const address = req.query.url
 
   // validate URL
-  if (UrlRegex.test(url)) {
-    analyzeWebsite(url)
+  if (UrlRegex.test(address)) {
+    analyzeWebsite(address)
       .then(result => {
         res.setHeader('Content-Type', 'application/json')
         res.send({
-          url,
+          url: address,
           ...result,
         })
       })
       .catch(error => {
+        console.log(error)
+
         res.setHeader('Content-Type', 'application/json')
         res.send({
-          success: false,
           statusCode: 500,
-          error,
+          error: {
+            message: 'An error occured while analyzing the website.',
+          },
         })
       })
   } else {
     res.setHeader('Content-Type', 'application/json')
     res.send({
-      success: false,
       statusCode: 500,
       error: {
         message: 'The URL you entered is not valid',
@@ -51,73 +56,95 @@ app.get('/', (req, res) => {
   }
 })
 
-function analyzeWebsite(uri) {
+async function analyzeWebsite(uri) {
   const options = {
     uri,
-    simple: false, // TODO: doesnt work yet
+    simple: false,
+    resolveWithFullResponse: true,
     time: true,
-    transform: (body, response) => [cheerio.load(body), response],
-    transform2xxOnly: true,
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
   }
 
-  return new Promise((resolve, reject) => {
-    rp(options)
-      .then(([$, response]) => {
-        let data = {}
+  const parsedURL = url.parse(uri, true)
+  const baseURL = `${parsedURL.protocol}//${parsedURL.host}`
+  // const fullURL = `${baseURL}${parsedURL.pathname}`
+  const response = await rp(options)
 
-        if (response.statusCode === 200) {
-          // Crawling successful
-          const images = $('img, picture')
-          const largestImagePath = images.length
-            ? images
-                .toArray()
-                .sort((a, b) => b.attribs.width * b.attribs.height - a.attribs.width * a.attribs.height)[0].attribs.src
-            : undefined
-          const largestImageURL = largestImagePath ? `${uri}${largestImagePath}` : null
+  // statusCode === 2xx
+  if (/^2/.test('' + response.statusCode)) {
+    const $ = cheerio.load(response.body)
 
-          const linksInternalElements = $('a:not([target="_blank"])')
-          const linksInternal = linksInternalElements.toArray().map(element => element.attribs.href)
+    // Images
+    const imageElements = $('img')
+    const imageURLs = imageElements.toArray().map(elem => elem.attribs.src)
+    const imageURLsParsed = imageURLs.map(imageURL => {
+      // const parsedImageURL = url.parse(imageURL, true)
+      // TODO: fix all edge cases
+      // if protocol missing, add 'https:'
+      if (imageURL.startsWith('//')) {
+        return `https:${imageURL}`
+      }
+      // if relative url, add baseURL
+      if (imageURL.startsWith('/')) {
+        return `${baseURL}${imageURL}`
+      }
 
-          const linksExternalElements = $('a[target="_blank"]')
-          const linksExternal = linksExternalElements.toArray().map(element => element.attribs.href)
+      return imageURL
+    })
 
-          const linksElements = $('a')
-          const linksInaccessible = linksElements.toArray().filter(element => element.attribs.href === undefined)
+    // get image size without full download
+    const imagesWithSizes = await Promise.all(imageURLsParsed.map(imageURL => probeImageSize(imageURL)))
+    const largestImageURL = imagesWithSizes.length
+      ? imagesWithSizes.sort((a, b) => b.width * b.height - a.width * a.height)[0].url
+      : null
 
-          data = {
-            success: true,
-            html: $.html(),
-            pageTitle: $('title').text(),
-            headings: {
-              heading1: $('h1').length,
-              heading2: $('h2').length,
-              heading3: $('h3').length,
-              heading4: $('h4').length,
-              heading5: $('h5').length,
-              heading6: $('h6').length,
-            },
-            imagesCount: images.length,
-            largestImageURL,
-            linksInternal,
-            linksExternal,
-            linksInaccessible,
-            loadingTime: response.elapsedTime,
-            statusCode: response.statusCode,
-          }
-        } else {
-          data = {
-            response,
-          }
-        }
+    // Links
+    const links = $('a[href]')
+      .toArray()
+      .map(element => element.attribs.href)
+    const linksInternal = links.filter(link => link.includes(baseURL) || link.startsWith('/'))
+    const linksExternal = links.filter(link => !link.includes(baseURL) && !link.startsWith('/'))
+    const brokenLinks = await checkBrokenLinks(baseURL, links)
+    const linksInaccessible = brokenLinks.top.map(link => link.url)
 
-        resolve(data)
-      })
-      .catch(error => {
-        // Request failed due to technical reasons...
-        // TEMP: add hardcoded 404 status
-        resolve({ success: false, statusCode: 404, error })
-      })
-  })
+    return {
+      html: $.html(),
+      pageTitle: $('title').text(),
+      headings: {
+        heading1: $('h1').length,
+        heading2: $('h2').length,
+        heading3: $('h3').length,
+        heading4: $('h4').length,
+        heading5: $('h5').length,
+        heading6: $('h6').length,
+      },
+      imagesCount: imageURLs.length,
+      largestImageURL,
+      linksInternal,
+      linksExternal,
+      linksInaccessible,
+      loadingTime: response.elapsedTime,
+      statusCode: response.statusCode,
+    }
+  } else if (response.statusCode === 404) {
+    return {
+      statusCode: response.statusCode,
+      // hardcoded because no error description provided by rp
+      error: {
+        message: '404 - Page not found',
+      },
+    }
+  } else {
+    return {
+      // for every other statusCode show generic message
+      statusCode: response.statusCode,
+      error: {
+        message: 'An unknown error occured',
+      },
+    }
+  }
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
